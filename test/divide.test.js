569f210c876d5d1e4d414312d0ab5eae3408f66f
kittybagpipes <sparticus978@gmail.com>
2020-11-29 06:25:29 -0800

    modified

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,1 +1,23 @@
 const divide = require("../index");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Divide function", () => {
+  it("should divide positive intergers correctly", () => {
+    const a = 8;
+    const b = 4;
+    const expectedAnswer = 2;
+
+    const actualAnswer = divide(a, b);
+    expect(actualAnswer).to.equal(expectedAnswer);
+  });
+  it("should throw an error when divide by zero", () => {
+    const a = 8,
+      b = 0;
+    const fn = () => {
+      divide(a, b);
+    };
+
+    expect(fn).to.throw();
+  });
+});
